0bf251341d41be6840779aaff41741dbbc320950
Huy0123 <voquochuysn2004@gmail.com>
2026-02-24 17:02:53 +0700

    edit database tour

--- src/modules/tour-details/entities/tour-detail.entity.ts
+++ src/modules/tour-details/entities/tour-detail.entity.ts
@@ -2,10 +2,13 @@ import {
   Column,
   Entity,
   JoinColumn,
+  OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Tour } from '@/modules/tours/entities/tour.entity';
+import { Review } from '@/modules/reviews/entities/review.entity';
+import { TourSession } from '@/modules/tour-session/entities/tour-session.entity';
 @Entity('tour_details')
 export class TourDetail {
   @PrimaryGeneratedColumn('uuid')
@@ -15,12 +18,27 @@ export class TourDetail {
   @JoinColumn({ name: 'tour_id' })
   tour: Tour;
 
-  @Column()
+  @Column('simple-array', { nullable: true })
+  moreInfo: string[];
+
+  @Column({ type: 'text', nullable: true })
+  experience: string;
+
+  @Column('simple-array', { nullable: true })
+  itinerary: string[];
+
+  @Column({ type: 'text', nullable: true })
   description: string;
 
-  @Column()
-  itinerary: string;
+  @Column('simple-array', { nullable: true })
+  images: string[];
 
-  @Column()
-  inclusions: string;
+  @Column({ type: 'text', nullable: true })
+  introduce: string;
+
+  @OneToMany(() => TourSession, (session) => session.tourDetail)
+  sessions: TourSession[];
+
+  @OneToMany(() => Review, (review) => review.tourDetail)
+  reviews: Review[];
 }
--- src/modules/tours/entities/tour.entity.ts
+++ src/modules/tours/entities/tour.entity.ts
@@ -5,16 +5,15 @@ import {
   Entity,
   Index,
   ManyToOne,
-  OneToMany,
+  ManyToMany,
+  JoinTable,
+  JoinColumn,
   OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { Location } from '@/modules/location/entities/location.entity';
 import { TourDetail } from '@/modules/tour-details/entities/tour-detail.entity';
-import { Review } from '@/modules/reviews/entities/review.entity';
-import { TourSession } from '@/modules/tour-session/entities/tour-session.entity';
-import { TourImage } from '@/modules/tour-images/entities/tour-image.entity';
 
 @Entity('tours')
 export class Tour {
@@ -31,17 +30,11 @@ export class Tour {
   guideService: string;
 
   @Column()
-  type: string; // Family, Adult Only, ...
-
-  @Column()
   image: string;
 
   @Index()
   @Column({ default: 0 })
   price: number;
-
-  @Column({ default: 0 })
-  discount: number;
 
   @Index()
   @Column({ type: 'float', default: 0 })
@@ -50,20 +43,23 @@ export class Tour {
   @Column({ default: 0 })
   reviewCount: number;
 
-  @ManyToOne(() => Location, (location) => location.tours)
-  location: Location;
+  @Column({ default: false })
+  isPopular: boolean;
+
+  @ManyToOne(() => Location, (location) => location.departureTours)
+  @JoinColumn({ name: 'departureLocationId' })
+  departureLocation: Location;
+
+  @ManyToMany(() => Location, (location) => location.destinationTours)
+  @JoinTable({
+    name: 'tour_destinations',
+    joinColumn: { name: 'tourId', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'locationId', referencedColumnName: 'id' },
+  })
+  destinations: Location[];
 
   @OneToOne(() => TourDetail, (detail) => detail.tourId, { cascade: true })
   detail: TourDetail;
-
-  @OneToMany(() => Review, (review) => review.tour, { cascade: true })
-  reviews: Review[];
-
-  @OneToMany(() => TourSession, (session) => session.tour)
-  sessions: TourSession;
-
-  @OneToMany(() => TourImage, (image) => image.tour, { cascade: true })
-  images: TourImage[];
 
   @CreateDateColumn({ type: 'timestamptz' })
   createdAt: Date;
